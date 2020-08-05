3240c1ba6a51f4c32c0682b119fd3a76896c9ec4
Tatiana Inama <tatii.oxo@gmail.com>
2020-08-05 09:57:55 +0200

    Created distributors page

---
+++ lib/distributors.tsx
@@ -0,0 +1,63 @@
+import DistributorsData from 'data/distributors.json';
+
+export type Group = "capital federal y GBA" | "interior del pais" | "internacional";
+
+const _data = DistributorsData as Distributor[];
+const _groups: Group[] = ["capital federal y GBA", "interior del pais", "internacional"];
+
+export type Distributor = {
+  group: Group | 'casa central';
+  country: string;
+  province: string;
+  place: string;
+  name: string;
+  address: string;
+  phone: string[];
+};
+
+export type ByGroups = {
+  [key in Group] : Distributor[]
+}
+
+export const getAllDistributors = () => {
+  return _data;
+}
+
+export const getDistributorsByGroup = () => {
+  return _data.reduce((groups, distributor) => {
+    const { group } = distributor;
+    return group === 'casa central' ? groups : {
+      ...groups,
+      [group]: [
+        ...groups[group] || [],
+        distributor
+      ]
+    }
+  }, {} as ByGroups);
+}
+
+export const getGroups = () => {
+  return _groups;
+}
+
+export const getHeadquarters = () => {
+  return _data.find(distributor => distributor.group === 'casa central') || {
+    group: "casa central",
+    country: "argentina",
+    province: "capital federal",
+    place: "capital federal",
+    name: "casa central",
+    address: "Leopoldo Marechal 914",
+    phone: [
+      "01141396860",
+      "01141396861"
+    ]
+  };
+}
+
+export default {
+  getAllDistributors,
+  getDistributorsByGroup,
+  getGroups,
+  getHeadquarters,
+};
--- pages/distribuidoras.tsx
+++ pages/distribuidoras.tsx
@@ -1,7 +1,235 @@
-const Distribuidoras = () => (
-  <div>
-    <h3>Distribuidoras</h3>
-  </div>
-);
+import { FunctionComponent, useState } from 'react';
+import { GetStaticProps, InferGetStaticPropsType } from 'next';
+
+import { getDistributorsByGroup, ByGroups, getGroups, Group, Distributor, getHeadquarters } from 'lib/distributors';
+
+import Section from 'components/section';
+
+
+type DistributionListProps = {
+  groupName: string,
+  distributors: Distributor[],
+  showCountry?: boolean,
+}
+const DistributionList: FunctionComponent<DistributionListProps> = ({ groupName, distributors, showCountry }) => {
+  const [ expand, toggleExpand ] = useState(false);
+  return (
+    <div className="mdl-shadow--2dp eba-distribution-list">
+      <div className="eba-group-header" onClick={() => toggleExpand(!expand)}>
+        <h5>{groupName}</h5>
+        <div>
+          <i className="material-icons">
+            {
+              expand ? 'keyboard_arrow_up' : 'keyboard_arrow_down'
+            }
+          </i>
+        </div>
+      </div>
+      <div className={`eba-group-container ${expand ? 'is-opened' : ''}`}>
+        <ul className="mdl-list">
+        {
+          distributors.map((distributor, key) => (
+            <li key={key} className="mdl-list__item mdl-list__item--three-line">
+              <span className="mdl-list__item-primary-content">
+                <span>{distributor.name} {showCountry ? `(${distributor.country})` : null}</span>
+                <span className="mdl-list__item-text-body">
+                  <small>{distributor.province}, {distributor.place}</small><br/>
+                  {distributor.address}
+                </span>
+              </span>
+              <a className="mdl-list__item-secondary-action" href={`tel:${distributor.phone[0]}`}><i className="material-icons">call</i></a>
+            </li>
+            ))
+          }
+        </ul>
+      </div>
+      <style jsx>{`
+        .eba-distribution-list {
+          margin-bottom: 16px;
+        }
+        .eba-group-header {
+          display: flex;
+          align-items: center;
+          padding: 16px;
+          box-sizing: border-box;
+          padding-bottom: 0;
+        }
+        .eba-group-header h5 {
+          font-size: 16px;
+          flex-grow: 2;
+          order: 0;
+          margin: 0;
+          font-weight: 400;
+          text-transform: uppercase;
+          color: var(--primaryColor);
+        }
+        .eba-group-container {
+          height: 0;
+          transform: scaleY(0);
+          transform-origin: top;
+          transition: transform 0s;
+        }
+        .eba-group-container.is-opened {
+          height: auto;
+          transform: scaleY(1);
+          transition: transform 200ms ease-in-out;
+        }
+        .eba-group-container .mdl-list li:not(:last-child) {
+          border-bottom: 1px solid var(--secondaryLightColor);
+        }
+      `}</style>
+    </div>
+  )
+}
+
+const Distribuidoras: FunctionComponent<InferGetStaticPropsType<typeof getStaticProps>> = ({ groups, distributors, headquarters }) => {
+  const [ active, setActive ] = useState<Group>(groups[0]);
+  return (
+    <div>
+      <Section color="muted" style={{backgroundImage: 'url(/hq.jpg)', backgroundPosition: 'center'}} size="xlarge" img>
+        <div className="eba-hq">
+          <h3>
+            Casa ESPECIFICOS<br/>
+            <small>Lugar de encuentro de profesionales de la estetica</small>
+          </h3>
+          <p>
+            <i className="material-icons">place</i>
+            <a target="_blank" href="https://www.google.com/maps/place/Espec%C3%ADficos+Buenos+Aires/@-34.6075338,-58.4378541,17z/data=!4m14!1m8!3m7!1s0x95bcca6c8dad9245:0x8985011c07c0b099!2sEspec%C3%ADficos+Buenos+Aires!8m2!3d-34.607582!4d-58.438056!14m1!1BCgIgAQ!3m4!1s0x95bcca6c8dad9245:0x8985011c07c0b099!8m2!3d-34.607582!4d-58.438056">
+              {headquarters.address}, {headquarters.place} - {headquarters.country}
+            </a>
+          </p>
+          <p className="eba-phones">
+            <i className="material-icons">phone</i>
+            {
+              headquarters.phone.map(phone => (
+                <a href={`tel:${phone}`}>{phone}</a>
+              ))  
+            }
+          </p>
+        </div>
+      </Section>
+      <div className="mdl-grid">
+        <div className="mdl-cell mdl-cell--hide-desktop mdl-cell--hide-tablet">
+          {
+            groups.map((groupName, key) => (
+              <DistributionList
+                key={key}
+                groupName={groupName}
+                distributors={distributors[groupName]}
+                showCountry={groupName === 'internacional'}
+              />
+            ))
+          }
+        </div>
+        <div className="mdl-cell mdl-cell--hide-phone mdl-cell--12-col">
+          <h4>Distribuidoras</h4>
+          <div className="mdl-tabs is-upgraded eba-distribution">
+            <div className="mdl-tabs__tab-bar">
+              {
+                groups.map((name, key) => (
+                <a key={key} className={`mdl-tabs__tab ${active === name ? 'is-active' : ''}`} onClick={() => setActive(name)}>
+                  {name}
+                  <span className="mdl-tabs__ripple-container"></span>
+                </a>
+                ))
+              }
+            </div>
+            <div className="mdl-tabs__panel is-active">
+              <table className="mdl-data-table mdl-shadow--2dp">
+                <thead>
+                  <tr>
+                    <th className="mdl-data-table__cell--non-numeric">Distribuidor</th>
+                    { active === 'internacional' ? (
+                      <th className="mdl-data-table__cell--non-numeric">Pais</th>
+                    ) : null }
+                    <th className="mdl-data-table__cell--non-numeric">Provincia</th>
+                    <th className="mdl-data-table__cell--non-numeric">Localidad</th>
+                    <th className="mdl-data-table__cell--non-numeric">Domicilio</th>
+                    <th className="mdl-data-table__cell--non-numeric">Telefono</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {
+                    distributors[active].map((distributor, key) => (
+                    <tr key={key}>
+                      <td className="mdl-data-table__cell--non-numeric">{distributor.name}</td>
+                      { active === 'internacional' ? (
+                        <td className="mdl-data-table__cell--non-numeric">{distributor.country}</td>
+                      ) : null }
+                      <td className="mdl-data-table__cell--non-numeric">{distributor.province}</td>
+                      <td className="mdl-data-table__cell--non-numeric">{distributor.place}</td>
+                      <td className="mdl-data-table__cell--non-numeric">{distributor.address}</td>
+                      <td className="mdl-data-table__cell--non-numeric">{distributor.phone.join(', ')}</td>
+                    </tr>
+                    ))
+                  }
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+      <style jsx>{`
+        .eba-hq {
+          color: var(--secondaryXLightColor);
+          text-align: center;
+        }
+
+        .eba-hq h3 {
+          line-height: 0.8;
+        }
+
+        .eba-hq p {
+          display: flex;
+          justify-content: center;
+          text-transform: capitalize;
+          font-size: 16px;
+          margin-bottom: 10px;
+        }
+
+        .eba-hq p a {
+          color: var(--secondaryXLightColor);
+        }
+
+        .eba-hq p a:hover {
+          text-decoration: underline;
+        }
+
+        .eba-hq p.eba-phones a {
+          margin-left: 10px;
+        }
+        .eba-distribution {
+          display: flex;
+          flex-direction: column;
+          width: calc(100% - 74px);
+          align-items: center;
+          justify-content: center;
+          padding: 42px;
+          padding-top: 0;
+        }
+
+        h4 {
+          text-align: center;
+          text-transform: uppercase;
+        }
+        .eba-distribution .mdl-tabs__tab-bar,
+        .eba-distribution .mdl-tabs__panel,
+        .eba-distribution .mdl-tabs__panel table {
+          width: 100%;
+        }
+      `}</style>
+    </div>
+  );
+}
+
+export const getStaticProps: GetStaticProps<{ distributors: ByGroups, groups: Group[], headquarters: Distributor}> = async () => {
+  return {
+    props: {
+      groups: getGroups(),
+      distributors: getDistributorsByGroup(),
+      headquarters: getHeadquarters()
+    }
+  }
+}
 
 export default Distribuidoras;
