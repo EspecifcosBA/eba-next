fd9aff6cb58011b7b0d8fba2bae6124636f261f8
Tatiana Inama <tatii.oxo@gmail.com>
2020-08-31 18:27:00 +0200

    Add body styles for Card component

--- components/card.tsx
+++ components/card.tsx
@@ -1,16 +1,32 @@
 import { FunctionComponent } from 'react';
+import cx from 'classnames';
+
+import Button from 'components/button';
 
 type CardProps = {
   title?: string,
   img?: string,
   suppText?: string,
+  flexCard?: boolean,
+  muted?: boolean,
   onClick?: (e: React.MouseEvent<HTMLElement>) => void,
+  actions?: {
+    label: string,
+    onClick: (e: React.MouseEvent<HTMLElement>) => void
+  }[]
 }
 
-const Card: FunctionComponent<CardProps> = ({ title, img, suppText, onClick, children }) => {
+const Card: FunctionComponent<CardProps> = ({ title, flexCard, img, suppText, onClick, actions, muted, children }) => {
+  const classnames = cx({
+    'eba-card': true,
+    'mdl-card': true,
+    'mdl-card--flex': flexCard,
+    'mdl-card--muted': muted,
+    'mdl-card--extended': !!children,
+  });
 
   return (
-    <div className="eba-card mdl-card" onClick={onClick}>
+    <div className={classnames} onClick={onClick}>
       {
         img && (
           <div className="mdl-card__media">
@@ -30,21 +46,39 @@ const Card: FunctionComponent<CardProps> = ({ title, img, suppText, onClick, chi
       </div>
       {
         children ? (
-          <div className="mdl-card__supporting-text">
+          <div className="mdl-card__body">
             {
               children
             }
           </div>
         ) : null
       }
-
+      {
+        actions ? (
+          <div className="mdl-card__actions">
+            {
+              actions.map((action, key) => (
+                <Button key={key} onClick={action.onClick}>{action.label}</Button>
+              ))
+            }
+          </div>
+        ) : null
+      }
       <style jsx>
       {`
         .eba-card.mdl-card {
           width: 100%;
           height: 400px;
-          background-color: var(--secondaryXLightColor);
           border: 1px solid #efefef;
+        }
+        
+        .eba-card.mdl-card.mdl-card--flex {
+          height: 100%;
+          min-height: 400px;
+        }
+
+        .eba-card.mdl-card.mdl-card--muted {
+          background-color: var(--secondaryXLightColor);
         }
 
         .eba-card.mdl-card:hover {
@@ -77,17 +111,35 @@ const Card: FunctionComponent<CardProps> = ({ title, img, suppText, onClick, chi
           align-items: baseline;
         }
 
+        .eba-card.mdl-card--extended > .mdl-card__title {
+          border: 0;
+        }
+
+        .eba-card.mdl-card.mdl-card--flex > .mdl-card__title {
+          flex: unset;
+        }
         .eba-card > .mdl-card__title > .mdl-card__title-text {
           font-size: 1.125rem;
           align-self: baseline;
         }
 
-        .eba-card .mdl-card__title > .mdl-card__supporting-text {
+        .eba-card .mdl-card__title .mdl-card__supporting-text {
           padding: 0;
           white-space: nowrap;
           overflow: hidden;
           text-overflow: ellipsis;
           text-transform: capitalize;
+        }
+
+        .eba-card .mdl-card__body {
+          color: rgba(0,0,0,.54);
+          font-size: 1rem;
+          line-height: 18px;
+          overflow: hidden;
+          padding: 16px;
+          flex: 1;
+          border-top: 1px solid #efefef;
+          border-bottom: 1px solid #efefef;
         }
       `}</style>
     </div>
